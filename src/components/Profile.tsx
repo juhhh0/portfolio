f2d4df1436676d20ec14cf4eaf43fffcefdb2a75
Joris Mansion <juhh@juhh-core.lan>
2024-05-27 12:35:52 +0200

    catch phrase

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,22 +10,28 @@ export default function Profile() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5, delay: 0.5 }}
-      className="flex flex-col gap-3 items-center"
+      className="flex flex-col items-center md:items-start md:flex-row-reverse gap-4 relative px-4"
     >
-      <div className="w-36 h-36 md:w-64 md:h-64 rounded-full bg-center bg-cover profile-picture"></div>
-      <h1>Joris Mansion</h1>
-      <h2>Développeur web basé à Paris</h2>
-      <ul className="flex flex-wrap gap-3 justify-center items-center">
-        <a href="https://www.linkedin.com/in/joris-mansion/" target="_blank">
-          <IconLabel icon="/icons/linkedin.svg" label="LinkedIn" />
-        </a>
-        <a href="https://github.com/juhhh0" target="_blank">
-          <IconLabel icon="/icons/github.svg" label="GitHub" />
-        </a>
-        <a href="mailto:joris.mansion@gmail.com">
-          <IconLabel icon="/icons/email.svg" label="joris.mansion@gmail.com" />
-        </a>
-      </ul>
+      <img  className="w-36 h-36 block md:w-64 md:h-64 rounded-full object-cover" src="/pic.jpeg" alt="" />
+      <div className="flex-1 flex gap-2 flex-col justify-center text-center md:text-start">
+        <h1>Joris Mansion</h1>
+        <h2>Développeur web basé à Paris</h2>
+        <ul className="flex flex-wrap gap-3 justify-center items-center md:justify-start">
+          <a href="https://www.linkedin.com/in/joris-mansion/" target="_blank">
+            <IconLabel icon="/icons/linkedin.svg" label="LinkedIn" />
+          </a>
+          <a href="https://github.com/juhhh0" target="_blank">
+            <IconLabel icon="/icons/github.svg" label="GitHub" />
+          </a>
+          <a href="mailto:joris.mansion@gmail.com">
+            <IconLabel
+              icon="/icons/email.svg"
+              label="joris.mansion@gmail.com"
+            />
+          </a>
+        </ul>
+        <p className="pt-6 md:pt-4">Développeur React depuis plus de 2 ans dont une grande partie au sein de Prodigious (Publicis Groupe) à développer de nombreux projets web pour des grands comptes.</p>
+      </div>
     </motion.section>
   );
 }
